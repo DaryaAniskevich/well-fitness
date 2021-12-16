302fe511ff181c5b3c78b8f7434c2d6610907ae2
Darya <darya.aniskevich@mail.ru>
2021-12-16 04:17:33 +0300

    search goods

--- src/js/search.js
+++ src/js/search.js
@@ -1,26 +1,15 @@
 const search = () => {
   const modal_active = "modal_active";
   const openBtn = document.querySelectorAll(".search-btn");
-  const searchBlock = document.querySelector(".search");
-  const closeBtn = searchBlock.querySelector(".search-form-button");
+  const searchModal = document.querySelector(".search");
+  const closeBtn = searchModal.querySelector(".search-form-button");
+  const input = searchModal.querySelector(".search-form__input");
+  const numberOfGoods = searchModal.querySelector(
+    ".search-result-header__number"
+  );
+  const searchResult = searchModal.querySelector(".search-result-main");
 
-  openBtn.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      searchBlock.classList.add(modal_active);
-    });
-  });
-  closeBtn.addEventListener("click", () => {
-    searchBlock.classList.remove(modal_active);
-  });
-
-  searchBlock.addEventListener("click", (e) => {
-    if (
-      e.target.classList.contains(modal_active) ||
-      e.target.classList.contains("modal-wrapper")
-    ) {
-      searchBlock.classList.remove(modal_active);
-    }
-  });
+  modal(searchModal, openBtn, closeBtn);
 
   window.addEventListener("scroll", () => {
     if (
@@ -30,6 +19,84 @@ const search = () => {
       searchBlock.classList.remove(modal_active);
     }
   });
+
+  const renderCards = (data) => {
+    searchResult.innerHTML = "";
+    data.forEach((item) => {
+      const div = document.createElement("div");
+      div.classList.add(
+        "search-result-main-item",
+        "good-card",
+        "good-card_search"
+      );
+      div.setAttribute("data-href", item.href);
+      div.addEventListener("click", () => {
+        window.location.href = div.dataset.href;
+      });
+
+      div.innerHTML = `
+      <img
+        src="./images/db/${item.img}"
+        alt="${item.name}"
+        class="good-card__image good-card__image_search"
+      />
+      <div class="good-card-description">
+        <h4 class="good-card__heading good-card__heading_search">
+        ${item.name}
+        </h4>
+        <div class="good-card-rating good-card-rating_search">Рейтинг ${'<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 0.5L7.91148 3.86908L11.7063 4.6459L9.09284 7.50492L9.52671 11.3541L6 9.752L2.47329 11.3541L2.90716 7.50492L0.293661 4.6459L4.08852 3.86908L6 0.5Z" fill="#F99808"/></svg>'.repeat(
+          Math.round(item.raiting)
+        )}</div>
+        <div class="good-card-bottom">
+          <div class="good-card-price">
+            <div class="good-card-price__item good-card-price__item_search">
+            ${item.discountPrice ? item.discountPrice : item.price} ₽
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+
+      searchResult.append(div);
+    });
+  };
+
+  const declOfNum = (n, text_forms) => {
+    n = Math.abs(n) % 100;
+    const n1 = n % 10;
+    if (n > 10 && n < 20) {
+      return text_forms[2];
+    }
+    if (n1 > 1 && n1 < 5) {
+      return text_forms[1];
+    }
+    if (n1 === 1) {
+      return text_forms[0];
+    }
+    return text_forms[2];
+  };
+
+  const searchGoods = (data) => {
+    input.addEventListener("input", () => {
+      const filteredData = data.filter((item) => {
+        return (
+          item.keywords.includes(input.value.toLowerCase()) ||
+          item.name.toLowerCase().includes(input.value.toLowerCase())
+        );
+      });
+      numberOfGoods.innerHTML = `${filteredData.length} ${declOfNum(
+        filteredData.length,
+        ["товар", "товара", "товаров"]
+      )}`;
+      renderCards(filteredData);
+    });
+  };
+
+  fetch(
+    "https://wellfitness-a4db3-default-rtdb.europe-west1.firebasedatabase.app/db/goods.json"
+  )
+    .then((res) => res.json())
+    .then((res) => searchGoods(res));
 };
 
 search();
